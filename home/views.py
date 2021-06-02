bb6ee9d244fb0f7ea8c55a31224f7ea2bfdfb828
Jeon316upzx <mamasbwoyjeon94@gmail.com>
2021-06-02 10:21:43 +0100

    added the lattitude and longitude

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+import requests
 
 
 def home(request):
-
-    return render(request, "home/index.html", {})
+    if request.method == "POST":
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64;"}
+        weather_request = requests.get(
+            "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=51.5&lon=-0.25",
+            headers=headers)
+    result = weather_request.content
+    return render(request, "home/index.html", {result: result})
